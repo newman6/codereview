14cbf587ae91dd8fd962902511bac4f1ed0e625a
newman6 <51217299+newman6@users.noreply.github.com>
2019-06-12 15:12:42 +0900

    Update shopping.cc

--- project2/shopping.cc
+++ project2/shopping.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+
 #include "./shopping.h"
 
 // Push Goods object to shopping_list_ vector
@@ -20,44 +22,55 @@ void Shopping::PrintList() {
   }
 }
 
+void Shopping::comparePrice(const Goods g1, const Goods g2) {
+  return g1.GetPrice > g2.GetPrice;
+}
 // Sort shopping_list_ Elements by Price
 // Apply Selection Sort algorithm
 void Shopping::SortByPrice() {
-  std::vector<Goods>::iterator p = shopping_list_.begin();
-  std::vector<Goods>::iterator min = shopping_list_.end();
-  for (std::vector<Goods>::iterator i = shopping_list_.begin();
-      i != shopping_list_.end()-1; i++) {
-    for (std::vector<Goods>::iterator j = i; j != shopping_list_.end(); j++) {
-      if (min == shopping_list_.end()) {
-        min = j;
-        continue;
-      }
-      if (min->GetPrice() > j->GetPrice()) {
-        min = j;
-      }
-    }
-    std::iter_swap(i, min);
-    min = shopping_list_.end();
-  }
+  sort(shopping_list_.begin(), shopping_list_.end(), comparePrice);
+  
+//  std::vector<Goods>::iterator p = shopping_list_.begin();
+//  std::vector<Goods>::iterator min = shopping_list_.end();
+//  for (std::vector<Goods>::iterator i = shopping_list_.begin();
+//      i != shopping_list_.end()-1; i++) {
+//    for (std::vector<Goods>::iterator j = i; j != shopping_list_.end(); j++) {
+//      if (min == shopping_list_.end()) {
+//        min = j;
+//        continue;
+//      }
+//      if (min->GetPrice() > j->GetPrice()) {
+//       min = j;
+//      }
+//    }
+//    std::iter_swap(i, min);
+//    min = shopping_list_.end();
+//  }
+}
+
+
+void Shopping::compareIndex(const Goods g1, const Goods g2) {
+  return g1.GetIndex > g2.GetIndex;
 }
 
 // Sort shopping_list_ Elements by Index
 // Apply Selection Sort algorithm
 void Shopping::SortByIndex() {
-  std::vector<Goods>::iterator p = shopping_list_.begin();
-  std::vector<Goods>::iterator min = shopping_list_.end();
-  for (std::vector<Goods>::iterator i = shopping_list_.begin();
-      i != shopping_list_.end()-1; i++) {
-    for (std::vector<Goods>::iterator j = i; j != shopping_list_.end(); j++) {
-      if (min == shopping_list_.end()) {
-        min = j;
-        continue;
-      }
-      if (min->GetIndex() > j->GetIndex()) {
-        min = j;
-      }
-    }
-    std::iter_swap(i, min);
-    min = shopping_list_.end();
+  sort(shopping_list_.begin(), shopping_list_.end(), compareIndex);
+//  std::vector<Goods>::iterator p = shopping_list_.begin();
+//  std::vector<Goods>::iterator min = shopping_list_.end();
+//  for (std::vector<Goods>::iterator i = shopping_list_.begin();
+//      i != shopping_list_.end()-1; i++) {
+//    for (std::vector<Goods>::iterator j = i; j != shopping_list_.end(); j++) {
+//      if (min == shopping_list_.end()) {
+//        min = j;
+//        continue;
+//      }
+//      if (min->GetIndex() > j->GetIndex()) {
+//        min = j;
+//      }
+//    }
+//    std::iter_swap(i, min);
+//    min = shopping_list_.end();
   }
 }
